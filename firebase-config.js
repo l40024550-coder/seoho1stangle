785ad2a250ea31d0dddecdbc705abd86090087f3
l40024550-coder <l40024550@gmail.com>
2026-09-15 21:18:36 +0900

    Firebase 설정 템플릿 추가

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 콘솔에서 만든 웹 앱의 설정값으로 교체하세요.
+// https://firebase.google.com/ 에서 프로젝트를 만든 뒤 Firestore를 활성화하면 됩니다.
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
