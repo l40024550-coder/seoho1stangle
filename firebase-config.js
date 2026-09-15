fd63aea7d61928b09b89a4635189b903878bc920
l40024550-coder <l40024550@gmail.com>
2026-09-15 21:43:46 +0900

    Connect Firebase web app configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Firebase 콘솔에서 만든 웹 앱의 설정값으로 교체하세요.
-// https://firebase.google.com/ 에서 프로젝트를 만든 뒤 Firestore를 활성화하면 됩니다.
+// Firebase Web App configuration
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBMKRrclbtXrsSRZW5h4jUoUMqVg6o8qxI",
+  authDomain: "seoho1angle.firebaseapp.com",
+  projectId: "seoho1angle",
+  storageBucket: "seoho1angle.firebasestorage.app",
+  messagingSenderId: "578868532521",
+  appId: "1:578868532521:web:aadaa38346ed7db8439338",
+  measurementId: "G-RJEMMJBCGX"
 };
